297fe72d3ce7d70a66711f4018f8f65e7b4f9cf6
1Dohyeon <dh1072005@gmail.com>
2024-04-06 21:14:20 +0900

    add : 예외 처리 및 로거 적용, db 적용

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,36 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exception/httpException.filter';
+import { QueryFailedFilter } from './common/exception/queryFailedFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT;
+
+  // 예외 처리 글로벌 설정
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalFilters(new QueryFailedFilter());
+
+  // class-validation 활용을 위한 등록
+  app.useGlobalPipes(new ValidationPipe());
+
+  // swagger 등록
+  const config = new DocumentBuilder()
+    .setTitle('RentEase')
+    .setDescription('user')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  // CORS
+  app.enableCors({
+    origin: true, // 배포할 때는 특정 url로 수정
+    credentials: true,
+  });
+
+  await app.listen(PORT);
 }
 bootstrap();
