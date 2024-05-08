a893b6259f2240c55a39ea728b2a6a5e69230af4
1Dohyeon <dh1072005@gmail.com>
2024-05-08 22:56:25 +0900

    add: 컨트롤러 엔드포인트 생성

--- server/src/setting/setting.controller.ts
+++ server/src/setting/setting.controller.ts
@@ -1,8 +1,25 @@
-import { Controller, Get, Redirect } from '@nestjs/common';
+import { Controller, Get, Patch, Redirect, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
 
 @Controller('settings')
+@UseGuards(JwtAuthGuard)
 export class SettingController {
   @Get('/')
   @Redirect('/settings/profile')
   async redirectToProfile() {}
+
+  @Get('/profile')
+  async getProfile() {}
+
+  @Patch('/profile')
+  async updateProfile() {}
+
+  @Get('/account')
+  async getUserAccount() {}
+
+  @Patch('/account/email')
+  async updateEmail() {}
+
+  @Patch('/account/password')
+  async updatePassword() {}
 }
