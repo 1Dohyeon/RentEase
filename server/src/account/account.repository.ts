4bf21cb21f38fe9b5d019dd698b40dd858a0268e
1Dohyeon <dh1072005@gmail.com>
2024-05-09 20:39:05 +0900

    comment: 메서드 설명 추가

--- server/src/account/account.repository.ts
+++ server/src/account/account.repository.ts
@@ -3,6 +3,10 @@ import { UserRepository } from 'src/user/user.repository';
 
 @Injectable()
 export class AccountRepository extends UserRepository {
+  /**
+   * 계정 정보만 불러옴(email, username)
+   * to SettingController.getAccountById
+   */
   async getAccountById(userId: number) {
     const user = await this.getUserById(userId);
 
@@ -11,7 +15,6 @@ export class AccountRepository extends UserRepository {
       throw new BadRequestException('해당하는 사용자를 찾을 수 없습니다.');
     }
 
-    // 사용자 정보 중에서 email과 name 속성만 반환
     return {
       nickname: user.email,
       name: user.username,
