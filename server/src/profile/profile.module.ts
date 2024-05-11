4318d47b669d04ee50854f95499277d1031c81c7
1Dohyeon <dh1072005@gmail.com>
2024-05-11 22:21:03 +0900

    add: profile service 생성

--- server/src/profile/profile.module.ts
+++ server/src/profile/profile.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/user/user.entity';
 import { UserModule } from 'src/user/user.module';
 import { ProfileRepository } from './profile.repository';
+import { ProfileService } from './profile.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity]), UserModule],
-  providers: [ProfileRepository],
+  providers: [ProfileRepository, ProfileService],
   exports: [ProfileRepository],
 })
 export class ProfileModule {}
