8e65dcfd4f3e382014982376ab2179c6ee90653a
1Dohyeon <dh1072005@gmail.com>
2024-05-18 13:15:17 +0900

    add: get article

--- server/src/article/article.repository.ts
+++ server/src/article/article.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { ArticleEntity } from './article.entity';
@@ -13,22 +13,34 @@ export class ArticleRepository extends Repository<ArticleEntity> {
   }
 
   async getArticles() {
-    return this.articleRepository.getArticles();
+    return await this.repository.find();
   }
 
   async getArticleById(articleId: number) {
-    return this.articleRepository.getArticleById(articleId);
+    try {
+      const article = await this.repository
+        .createQueryBuilder('article')
+        .where('article.id = :id', { id: articleId })
+        // password 제외하고 불러옴
+        .select(['article.title', 'article.createAt'])
+        .getOne();
+
+      if (!article) throw new Error();
+      return article;
+    } catch (error) {
+      throw new BadRequestException('해당하는 사용자를 찾을 수 없습니다.');
+    }
   }
 
   async createArticle() {
-    return this.articleRepository.createArticle();
+    return;
   }
 
   async updateArticle(articleId: number) {
-    return this.articleRepository.updateArticle(articleId);
+    return;
   }
 
   async deleteArticle(articleId: number) {
-    return this.articleRepository.deleteArticle(articleId);
+    return;
   }
 }
