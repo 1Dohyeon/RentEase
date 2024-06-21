7cae8ad80c7f06c7032356c366437245b2c921d1
1Dohyeon <dh1072005@gmail.com>
2024-06-21 16:50:27 +0900

    DELETE : 불필요한 서비스 파일 삭제

--- server/src/address/address.module.ts
+++ server/src/address/address.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AddressEntity } from './address.entity';
 import { AddressRepository } from './address.repository';
-import { AddressService } from './address.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AddressEntity])],
-  providers: [AddressService, AddressRepository],
-  exports: [AddressService, AddressRepository],
+  providers: [AddressRepository],
+  exports: [AddressRepository],
 })
 export class AddressModule {}
