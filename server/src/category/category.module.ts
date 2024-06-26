9b69efbce0a3efd7111bbc120726172d30c9ad1d
1Dohyeon <dh1072005@gmail.com>
2024-06-26 18:18:29 +0900

    ADD: 기본 카테고리 생성

--- server/src/category/category.module.ts
+++ server/src/category/category.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryEntity } from 'src/models/category.entity';
+import { CategoryRepository } from './category.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CategoryModule])],
+  imports: [TypeOrmModule.forFeature([CategoryEntity])],
+  providers: [CategoryRepository],
+  exports: [CategoryRepository],
 })
 export class CategoryModule {}
