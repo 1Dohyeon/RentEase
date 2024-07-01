562a36c131d6121505ce57d60241fe0cb19666a0
1Dohyeon <dh1072005@gmail.com>
2024-07-01 13:36:08 +0900

    RENAME: 파일 이름 변경

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Articles from "./pages/Articles";
+import ArticleDetailPage from "./pages/ArticleDetailPage";
+import ArticlesPage from "./pages/ArticlesPage";
 import MyPage from "./pages/MyPage";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
@@ -10,7 +11,8 @@ const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/articles" />} />
-      <Route path="/articles" element={<Articles />} />
+      <Route path="/articles" element={<ArticlesPage />} />
+      <Route path="/articles/:id" element={<ArticleDetailPage />} />
       <Route path="/auth/login" element={<Login />} />
       <Route path="/auth/register" element={<Register />} />
       <Route path="/users/:userId" element={<MyPage />} />
