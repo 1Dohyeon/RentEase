d0973005e1114f9052ca09c49c67ce8db5eead3e
1Dohyeon <dh1072005@gmail.com>
2024-07-03 17:35:00 +0900

    ADD: 글 작성을 위한 모든 카테고리 조회 기능 추가

--- server/src/category/category.module.ts
+++ server/src/category/category.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryEntity } from 'src/models/category.entity';
 import { CategoryRepository } from './category.repository';
+import { CategoryController } from './category.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CategoryEntity])],
   providers: [CategoryRepository],
   exports: [CategoryRepository],
+  controllers: [CategoryController],
 })
 export class CategoryModule {}
