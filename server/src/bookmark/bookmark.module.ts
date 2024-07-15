9d6d3dcfc8cfd51504d62fe34f4ab31d94261dfd
1Dohyeon <dh1072005@gmail.com>
2024-07-15 19:03:03 +0900

    ADD: 북마크 컨트롤러 생성 및 테스트 폴더 생성 후 테스트 파일 이동

--- server/src/bookmark/bookmark.module.ts
+++ server/src/bookmark/bookmark.module.ts
@@ -4,10 +4,12 @@ import { BookmarkEntity } from 'src/models/bookmark.entity';
 import { UserModule } from 'src/user/user.module';
 import { BookmarkRepository } from './bookmark.repository';
 import { BookmarkService } from './bookmark.service';
+import { BookmarkController } from './bookmark.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([BookmarkEntity]), UserModule],
   providers: [BookmarkService, BookmarkRepository],
   exports: [BookmarkService, BookmarkRepository],
+  controllers: [BookmarkController],
 })
 export class BookmarkModule {}
